10d6cdcee013f19334945c322e627a35e9a2ae9b
Pete <peter.gammon@oxfordcc.co.uk>
2021-08-06 15:57:45 +0100

    Updated to add timeout so can record change and update ui

--- src/app/utility/mc-enumeration-list-with-category/mc-enumeration-list-with-category.component.ts
+++ src/app/utility/mc-enumeration-list-with-category/mc-enumeration-list-with-category.component.ts
@@ -52,9 +52,9 @@ export class McEnumerationListWithCategoryComponent implements OnInit {
   enumsCount: number;
   total: number;
   displayItems: any[];
-  categories: any[] = [];
-  allRecords: any[];
-  allRecordsWithGroups: any[];
+  categories = [];
+  allRecords = [];
+  allRecordsWithGroups = [];
 
   hasCategory = false;
   showEdit = false;
